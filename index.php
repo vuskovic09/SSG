72d6447df9763c04fcf23aff6947d789a3daef44
Filip Vušković <filip.vuskovic09@gmail.com>
2020-10-13 21:10:09 +0200

    ADD: base files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+ob_start();
+session_start();
+
+require('include/services/config.php');
+require('include/services/connection.php');
+
+?>
+
+<?php require_once('include/partials/header.php'); ?>
+
+<?php require_once('include/partials/main.php'); ?>
+
+<?php require_once('include/partials/footer.php'); ?>
+
+</body>
+</html>
