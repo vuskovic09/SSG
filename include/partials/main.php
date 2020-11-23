3c10459f454b44d87362c8e8764e09d2710da06d
Filip Vušković <filip.vuskovic09@gmail.com>
2020-11-24 00:51:07 +0100

    ADD: text + css

--- include/partials/main.php
+++ include/partials/main.php
@@ -1,15 +1,19 @@
 <main>
     <div class="main container">
-        <div class="main-segment">
+
+    
+
+        <!-- Intro -->
+        <div class="main-block">
             <div class="row center">
                 <div class="col-md-12">
                     <div class="main-segment-title">
                         <h2>Uvodna riječ</h2>
                     </div>
                 </div>
-            </div>  
-            <div class="main-segment-about">
-                <div class="main-segment-about-photos">
+            </div>
+            <div class="row center main-segment-about">
+                <div class="col-md-4 main-segment-about-photos">
                     <!-- First Img -->
                     <div class="first-img">
                         <img src="assets/images/about1.jpg" alt="Photo by Carsten Kohler from Pexels">
@@ -23,23 +27,191 @@
                         <img src="assets/images/about3.jpg" alt="Photo by Pixabay from Pexels">
                     </div>
                 </div>
-                <div class="main-segment-about-text">
+                <div class="col-md-8 main-segment-about-text">
+                    <p>Dragi natjecatelji,</br></br>
+                        Organizator MUSICA ZAGREB sa velikim zadovoljstvom najavljuje i želi
+                        dobrodošlicu svim natjecateljicama i natjecateljima na 1. Pjevačkom natjecanju
+                        „STOJAN STOJANOV GANČEV“</br>
+                        Grad Zagreb, domaćin ovog međunarodnog događanja glavni je grad Republike
+                        Hrvatske ali i znanstveno i gospodarsko središte ovog dijela Europe. To je grad
+                        kulture koji se ponosi svojim brojnim kulturnim i umjetničkim aktivnostima.
+                        Treba spomenuti da se godišnje prosječno realizira 230 koncerata, 550
+                        izložbaba, 270 festivala i sa ostalim vidovima kulturne baštine prelazi
+                        impozantnu brojku od 5000 događanja.</br>
+                        Ovo prvo natjecanje posvećeno je In memoriam velikom bugarskom pjevaču i
+                        pjevačkom pedagogu Stojanu Stojanovu Gančevu koji je ostavio dubok trag u
+                        našoj sredini.</br>
+                        Zamisao i želja organizatora MUSICA ZAGREB jest ostvarenje natjecanja za
+                        talentirane perspektivne mlade umjetnike širom svijeta koji će dobiti priliku da
+                        putem ONLINE tehnologije prezentiraju svoje umijeće, vještine i ljubav prema
+                        glazbi.</br>
+                        Veliki i čuveni austrijski skladatelj Franz Joseph Haydn inspirirao nas je svojim
+                        riječima:</br>
+                        „Mladi ljudi mogu naučiti iz mog primjera da nešto ne može doći ni iz čega. Ono
+                        što sam postigao rezultat je mog velikog napora, truda i ljubavi“.</br>
+                        Svim učesnicima želimo mnogo uspjeha i sreće od srca,te Vas s nestrpljenjem
+                        očekujemo na predstojećem natjecanju.</br></br>
+                        Srdačno,</br>
+                        Organizator MUSICA ZAGREB 
+                    </p>
+                </div>
+            </div>  
+        </div>
+
+        <!-- Gallery -->
+        <div class="main-block">
+            <div class="row center main-segment">
+                <div class="col-md-12 main-segment-gallery">
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                    <div class="hex"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-block">
+            <div class="row center">
+                <div class="col-md-12">
+                    <div class="main-segment-title">
+                        <h2>Pjevačko natjecanje</h2>
+                    </div>
+                </div>
+            </div> 
+            <div class="row center main-segment-comp">
+                <div class="col-md-12 main-segment-comp-text">    
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ullam at incidunt quidem sunt enim magni fuga nisi illum exercitationem repudiandae, itaque officiis quam nam, pariatur inventore sit veniam modi!</p>
                 </div>
             </div>
         </div>
-        <div class="main-segment">
-            <div class="main-segment-gallery">
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
-                <div class="hex"></div>
+
+        <!-- Rules -->
+        <div class="main-block">
+            <div class="row center">
+                <div class="col-md-12">
+                    <div class="main-segment-title">
+                        <h2>Pravilnik natjecanja</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row center main-segment-comp">
+                <div class="col-md-12 main-segment-comp-text">    
+                    <p>
+                        PRAVILNIK NATJECANJA STOJAN STOJANOV GANČEV</br></br>
+                        1. Natjecanje je namijenjeno mladim glazbenicima u Republici Hrvatskoj i mladim
+                        glazbenicima u inozemstvu bez obzira na vrstu obrazovanja i nacionalnost.</br>
+                        2. Čitavo natjecanje Stojan Stojanov Gančev će se odvijati – ON LINE.</br>
+                        3. Natjecatelji su dužni do trenutka popunjavanja prijave objaviti program koji izvode na
+                        ovom natjecanju na javnom youtube kanalu i u prijavi navesti link na predviđenom mjestu
+                        za svaku pojedinu skladbu.</br>
+                        4. Skladbe mogu biti snimljene pojedinačno. Takoder, program može biti snimljen i u
+                        cijelosti, bez prekidanja.</br>
+                        5. Snimljene skladbe ne smiju biti objavljene na javnom youtube kanalu prije 1. siječnja
+                        2019. godine. Obvezatno je navesti ime skladbe u opisu videa.</br>
+                        6. Natjecatelj/ica je dužan dostaviti potvrdu od nadležne institucije ili svog pjevačkog
+                        pedagoga o godinama učenja pjevanja.</br>
+                        7. Redoslijed nastupa određuje organizator i zadržava pravo promjene rasporeda.</br>
+                        8.Točan termin rasporeda natjecanja u svakoj pojedinoj kategoriji bit će objavljen na
+                        internetskoj stranici Natjecanja a natjecatelji će također biti obaviješteni putem e-maila.</br>
+                        9. Natjecatelj ili komorni ansambl se može prijaviti i u višoj kategoriji nego je predviđena za
+                        njihovu dob.</br>
+                        10. Cijeli program natjecatelj izvodi napamet u solističkim kategorijama , a komorni
+                        ansambli mogu izvoditi program iz nota, a repeticije nisu nužne.</br>
+                        11. Promjena programa navedenog u prijavi neće biti dopuštena.</br>
+                        12. Prijavom na natjecanje natjecatelji daju privolu Organizatoru da može koristiti osobne
+                        podatke u svim dostavljenim dokumentima isključivo u svrhu Natjecanja.</br>
+                        13. Ocjenjivački sud sastavljen je od eminentnih domaćih i stranih glazbenika i pedagoga.</br>
+                        14. U slučaju odustajanja pojedinog člana ocjenjivačkog suda, Organizator ima pravo
+                        zamjene istog novim članom.</br>
+                        15. Član Ocjenjivačkog suda ne ocjenjuje svog učenika ili studenta koji se natječe, ni
+                        natjecatelja s kojim je u srodstvu.</br>
+                        16. Ocjenjivački sud sluša program u cijelosti.</br>
+                        17. U slučaju da više natjecatelja dijeli jednak broj bodova, mlađi kandidat ostvaruje pravo
+                        prednosti.</br>
+                        18. Ocjenjivački sud donosi konačnu i neopozivu odluku na koju natjecatelj nema pravo
+                        žalbe.</br>
+                        19. Ocjenjivački sud ima mogućnost prema vlastitoj procjeni dodijeliti i posebne nagrade
+                        kao i nagradu najuspješnijem pedagogu.</br>
+                        20. Rezultati i pojedinačne ocjene svakog člana Ocjenjivačkog suda bit će objavljeni na ovoj
+                        web stranici po završetku svake kategorije.</br></br>
+                        Na Natjecanju ocjenjivački sud dodjeljuje nagrade:</br>
+                        90,00 – 100,00 bodova - 1. nagrada</br>
+                        80,00 – 89,99 bodova - 2. nagrada</br>
+                        70,00 – 79,99 bodova - 3. nagrada</br>
+                        69,99 bodova i sve manje od toga - Priznanje</br></br>
+                        21. Natjecatelji dobivaju diplome za osvojenu 1., 2. ili 3. nagradu ili priznanje za
+                        sudjelovanje.</br>
+                        22. Svaka kategorija ima svog pobjednika. To je natjecatelj/ica sa najvećim ostvarenim
+                        bodovima od strane Ocjenjivačkog suda. Ako je natjecatelj/ica ocijenjen sa više od 98,00
+                        bodova, dobiva naziv laureat.</br>
+                        23. Diplome i priznanja kao i posebne nagrade, Organizator natjecanja će svakom
+                        natjecatelju poslati na mail adresu navedenu u prijavi.</br>
+                        24. Ocjenjivački sud odlučuje o svemu što nije određeno ovim Pravilnikom.</br>
+                        25. Za sudjelovanje na natjecanju obvezna je prijava.</br>
+                        26.Prijava se podnosi isključivo popunjavanjem online prijavnice koja je dostupna na ovoj
+                        web stranici.</br>
+                        27. Prijave će biti omogućene od 15. studenog 2020.
+                        Zadnji rok za prijavu na natjecanje je 20. siječnja 2021.</br>
+                        28. Kotizacije po kategorijama su:</br>
+                        Pretkategorija A – 35 €</br>
+                        Pretkategorija B – 35 €</br>
+                        I/A kategorija – 40 €</br>
+                        I /B kategorija – 40 €</br>
+                        II/A kategorija – 40 €</br>
+                        II/B kategorija – 40 €</br>
+                        III/A kategorija – 50 €</br>
+                        III/B kategorija - 50 €</br>
+                        IV kategorija - 50 €</br>
+                        V/A kategorija – 60 €</br>
+                        V/B kategorija - 60 €</br>
+                        VI kategorija – 60 €</br>
+                        VII kategorija – 60 €</br>
+                        VIII kategorija - 60 €</br>
+                        29. Uplata kotizacije vrši se na račun Organizatora natjecanja MUSICA ZAGREB:</br>
+                        IBAN:</br>
+                        SWIFT:</br>
+                        30. U slučaju odustajanja kotizacija se ne vraća.</br>
+                        31. Krivo navođenje podataka u prijavi može biti razlog za diskvalifikaciju natjecatelja.</br>
+                        32. Nepotpune prijave neće se razmatrati.</br>
+                        33. U opisu plaćanja obavezno napisati ime i prezime natjecatelja, odnosno imena i
+                        prezimena svih natjecatelja u komornom ansamblu za koje se uplaćuje kotizacija.</br>
+                        34. Natjecatelji iz Republike Hrvatske uplaćuju kunsku protuvrijednost prema središnjem
+                        tečaju HNB na dan uplate.</br>
+                        35. Prijavom na natjecanje i slanjem prijave natjecatelji prihvaćaju sva pravila iz ovog
+                        Pravilnika natjecanja.</br>
+                        36. Organizator natjecanja zadržava pravo na audio/video/foto zapis svih događanja
+                        vezanih za natjecanje.</br>
+                        37. MUSICA ZAGREB, organizator Natjecanja, zadržava pravo izmjene ovog Pravilnika.</br>
+                        38. U slučaju nejasnoća hrvatski tekst uzima se kao pravovaljan.</br>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Judges -->
+        <div class="main-block">
+            <div class="row center">
+                <div class="col-md-12">
+                    <div class="main-segment-title">
+                        <h2>Ocijenjivački Sud</h2>
+                    </div>
+                </div>
+            </div>
+            <div class= "row center between main-segment-judges">
+                <div class="col-md-4 main-segment-judges-single">
+                    <div class="main-segment-judges-single-img">
+                        <img src="assets/images/personIMG.jpg" alt="Photo by LinkedIn Sales Navigator from Pexels" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
